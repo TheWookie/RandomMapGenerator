a54285edbdf6ef76881c2416734aeddb2d8959ac
Paul Baker <=>
2014-12-23 16:10:16 -0700

    Hex cell primitive

---
+++ src/main/java/experimental/iteration2/HexCell.java
@@ -0,0 +1,25 @@
+package experimental.iteration2;
+
+import java.util.List;
+
+public class HexCell extends MapCell {
+
+  @Override
+  public ImmutablePoint getCoordinates() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<MapCell> getAdjacentCells() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void clearAdjacentCells() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
