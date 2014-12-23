72546472759cc251e91d2028f87eb595e1ce9f14
Paul Baker <=>
2014-12-23 16:32:20 -0700

    Pulled it all together

--- src/main/java/io/paulbaker/generator/PerlinNoiseGenerator.java
+++ src/main/java/io/paulbaker/generator/PerlinNoiseGenerator.java
@@ -2,6 +2,13 @@ package io.paulbaker.generator;
 
 import java.util.Random;
 
+/**
+ * This is based on the implementation online here: <a
+ * href="http://freespace.virgin.net/hugo.elias/models/m_perlin.htm">
+ * http://freespace.virgin.net/hugo.elias/models/m_perlin.htm</a>
+ * 
+ * @author paulbaker
+ */
 public class PerlinNoiseGenerator {
 
   private static final Random RAND;
@@ -11,21 +18,37 @@ public class PerlinNoiseGenerator {
 
   private PerlinNoiseGenerator() {}
 
-  public static void generate(int width, int length, long seed) {
+  public static double[][] generate(int width, int length, long seed) {
     RAND.setSeed(seed);
-    generate(width, length);
+    return generate(width, length);
   }
 
-  public static void generate(int width, int length) {
-
+  public static double[][] generate(int width, int length) {
+    if (width == 0 || length == 0)
+      throw new IllegalArgumentException("You cannot have a 0 length or width");
+    // Initialize the two edges of the field
+    double[][] field = new double[width][length];
+    for (int i = 0; i < field.length; i++) {
+      field[i][0] = RAND.nextDouble();
+    }
+    for (int i = 0; i < field[0].length; i++) {
+      field[0][i] = RAND.nextDouble();
+    }
+    // Interpolate all the points in between
+    for (int i = 1; i < field.length; i++) {
+      for (int j = 1; j < field[i].length; j++) {
+        field[i][j] = interpolateCosine(field[0][i], field[j][0], 0.5);
+      }
+    }
+    return field;
   }
 
-  private double interpolateLinear(double a, double b, double x) {
+  private static double interpolateLinear(double a, double b, double x) {
     checkInterpolateX(x);
     return a * (1 - x) + b * x;
   }
 
-  private double interpolateCosine(double a, double b, double x) {
+  private static double interpolateCosine(double a, double b, double x) {
     checkInterpolateX(x);
     double ft = x * Math.PI;
     double f = (1 - Math.cos(ft)) * 0.5;
@@ -37,7 +60,7 @@ public class PerlinNoiseGenerator {
   // return 0f;
   // }
 
-  private void checkInterpolateX(double x) {
+  private static void checkInterpolateX(double x) {
     if (x < 0 || x > 1)
       throw new IllegalArgumentException("X must be between [0, 1]");
   }
