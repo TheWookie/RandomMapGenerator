a1b82b46864f45bc06d64cd9cf7757b0e7500b45
Paul Baker <=>
2014-12-23 16:18:44 -0700

    Filling implementation, but commenting out cubic interpolation

--- src/main/java/io/paulbaker/generator/PerlinNoiseGenerator.java
+++ src/main/java/io/paulbaker/generator/PerlinNoiseGenerator.java
@@ -20,15 +20,25 @@ public class PerlinNoiseGenerator {
 
   }
 
-  private float interpolateLinear(int a, int b, int x) {
-    return 0f;
+  private double interpolateLinear(double a, double b, double x) {
+    checkInterpolateX(x);
+    return a * (1 - x) + b * x;
   }
 
-  private float interpolateCosine(int a, int b, int x) {
-    return 0f;
+  private double interpolateCosine(double a, double b, double x) {
+    checkInterpolateX(x);
+    double ft = x * Math.PI;
+    double f = (1 - Math.cos(ft)) * 0.5;
+    return a * (1 - f) + b * f;
   }
 
-  private float interpolateCubic(int a, int b, int x) {
-    return 0f;
+  // private double interpolateCubic(double a, double b, double x) {
+  // checkInterpolateX(x);
+  // return 0f;
+  // }
+
+  private void checkInterpolateX(double x) {
+    if (x < 0 || x > 1)
+      throw new IllegalArgumentException("X must be between [0, 1]");
   }
 }
